805205a2352508d19305771e04a428a964e47676
Willem P. Junker <junkerwi@cs.grinnell.edu>
2017-02-20 16:31:28 -0600

    02-21 RE

---
+++ 02-21/binary-rep.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+// given unsigned char 153, its binary representation is 0b10011001
+
+// signed 0b10011001 is interpreted as -25
+
+// ™ (trademark) (http://www.rapidtables.com/code/text/ascii-table.htm)
+
+/* given "unsigned char x = 153", what is stored in x? :
+ 
+0b10011001 
+--- unsigned, so the highest place represents a number, not postivity/negativity
+--- 
